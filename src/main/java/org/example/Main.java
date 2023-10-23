f2a9a295dd37ba13736bbf00c71bc7444eab0a6e
yashk0311 <yash.koushik@iiitb.org>
2023-10-23 13:45:20 +0530

    Changed the calculator program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,36 @@
 package org.example;
 import java.util.Scanner;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public long multiply(int a, int b) {
+        return (long) a * b;
+    }
+
+    public float divide(int a, int b) {
+        if (b != 0) {
+            return (float) a / b;
+        } else {
+            System.out.println("Error: Division by zero.");
+            return 0;
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome to the Calculator Program!");
-        System.out.println();
-        while(true)
-        {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello and welcome to the Calculator Program!");
+        while (true) {
             System.out.println("Choose from the below operations: ");
             System.out.println("1. Addition");
             System.out.println("2. Subtraction");
@@ -17,24 +38,16 @@ public class Main {
             System.out.println("4. Division");
             System.out.println("5. Exit");
 
-            Scanner scanner = new Scanner(System.in);
             int option = scanner.nextInt();
             int a, b;
-            switch (option){
+            switch (option) {
                 case 1:
                     System.out.println("Please enter 1st operand: ");
                     a = scanner.nextInt();
                     System.out.println("Please enter 2nd operand: ");
                     b = scanner.nextInt();
-
-
                     System.out.println("-------------- Result ------------- ");
-                    System.out.print(a);
-                    System.out.print(" + ");
-                    System.out.print(b);
-                    System.out.print(" = ");
-                    System.out.print(a+b);
-                    System.out.println();
+                    System.out.println(a + " + " + b + " = " + calculator.add(a, b));
                     System.out.println("------------------------------------");
                     break;
 
@@ -43,14 +56,8 @@ public class Main {
                     a = scanner.nextInt();
                     System.out.println("Please enter 2nd operand: ");
                     b = scanner.nextInt();
-
                     System.out.println("-------------- Result ------------- ");
-                    System.out.print(a);
-                    System.out.print(" - ");
-                    System.out.print(b);
-                    System.out.print(" = ");
-                    System.out.print(a-b);
-                    System.out.println();
+                    System.out.println(a + " - " + b + " = " + calculator.subtract(a, b));
                     System.out.println("------------------------------------");
                     break;
 
@@ -59,14 +66,8 @@ public class Main {
                     a = scanner.nextInt();
                     System.out.println("Please enter 2nd operand: ");
                     b = scanner.nextInt();
-
                     System.out.println("-------------- Result ------------- ");
-                    System.out.print(a);
-                    System.out.print(" x ");
-                    System.out.print(b);
-                    System.out.print(" = ");
-                    System.out.print((long)a*b);
-                    System.out.println();
+                    System.out.println(a + " x " + b + " = " + calculator.multiply(a, b));
                     System.out.println("------------------------------------");
                     break;
 
@@ -75,19 +76,16 @@ public class Main {
                     a = scanner.nextInt();
                     System.out.println("Please enter 2nd operand: ");
                     b = scanner.nextInt();
-
                     System.out.println("-------------- Result ------------- ");
-                    System.out.print(a);
-                    System.out.print(" / ");
-                    System.out.print(b);
-                    System.out.print(" = ");
-                    System.out.print((float)a/b);
-                    System.out.println();
-                    System.out.println("------------------------------------");
+                    System.out.println(a + " / " + b + " = " + calculator.divide(a, b));
+                    System.out.println("-----------------------------------");
                     break;
+
                 case 5:
                     return;
+
                 default:
+                    System.out.println("Invalid option. Please choose a valid operation.");
                     break;
             }
         }
